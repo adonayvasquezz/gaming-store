07ea48ad37756ad91f8a802465e33107c9bcdbbb
Adonay Vasquez <adonay16.vz@gmail.com>
2021-08-23 00:10:31 -0600

    Adding react router dom

---
+++ src/pages/Auth.js
@@ -0,0 +1,12 @@
+import AuthForm from "../components/AuthForm";
+
+const Auth = () => {
+    return (
+        <>
+            <h1>Auth page</h1>
+            <AuthForm />
+        </>
+    )
+}
+
+export default Auth;
