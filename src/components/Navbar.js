9fb025be1ddba227d890594b83c9d8f95b2703cf
Adonay Vasquez <adonay16.vz@gmail.com>
2021-08-24 22:01:03 -0600

    Conditional navbar render if user is authenticated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,25 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../store/auth-context";
 
 const Navbar = () => {
+    const authCtx = useContext(AuthContext);
+    const isLoggedIn = authCtx.isLoggedIn;
+
     return (
         <div>
             <nav className="d-flex justify-content-between">
                 <div className="d-flex justify-content-start">
-                    <NavLink exact to="/" className="inherit" activeClassName="pageSelected">Page 1</NavLink>
-                    <NavLink to="/abc" className="inherit" activeClassName="pageSelected">Page 2</NavLink>
+                    { isLoggedIn && 
+                        <NavLink exact to="/" className="inherit" activeClassName="pageSelected">Page 1</NavLink>
+                    }
+                    { isLoggedIn && 
+                        <NavLink to="/abc" className="inherit" activeClassName="pageSelected">Page 2</NavLink>
+                    }
                 </div>
-                <NavLink to="/auth" className="inherit" activeClassName="pageSelected">Auth</NavLink>
+                { !isLoggedIn && 
+                    <NavLink to="/auth" className="inherit" activeClassName="pageSelected">Auth</NavLink>}
+               
             </nav>
         </div>
     )
