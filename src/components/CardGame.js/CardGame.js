d741ba1550456548d4b783dca44c9823199e4a57
Adonay Vasquez <adonay16.vz@gmail.com>
2021-08-27 00:22:25 -0600

    CardGame component Styles modifications

--- src/components/CardGame.js/CardGame.js
+++ src/components/CardGame.js/CardGame.js
@@ -6,19 +6,19 @@ const CardGame = ({ game }) => {
     //let gameImg = game.thumb ? game.thumb : noImg;
     return (
         <div className="card">
-            <div >
+            <div>
                 <img src={game.thumb} className={styles.imgCover} alt={game.internalName}></img>
             </div>
             <div className="card-body d-flex justify-content-between flex-column">
-                <div className="card-text text-center pt-1">{game.title}</div>
+                <h6 className="card-text text-center pt-2">{game.title}</h6>
 
                 <div className="d-flex justify-content-end align-items-center">
                     <div className="me-2">
-                        <label className="bg-success p-1 text-white rounded">
+                        <label className="bg-success p-2 text-white rounded">
                             -{Math.round(game.savings)}%
                         </label>
                     </div>
-                    <div className="pe-1 pb-1">
+                    <div className="pe-3 pb-2">
                         <div className={styles.normalPrice}>$ {game.normalPrice}</div>
                         <div className={styles.salePrice}>$ {game.salePrice}</div>
                     </div>
